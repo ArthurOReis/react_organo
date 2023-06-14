c1935efb5cc617df8049f8201830d1a93d085a42
ArthurOReis <arthuroreis2005@gmail.com>
2023-06-14 18:15:08 -0300

    Adicionando cards em times

--- src/App.js
+++ src/App.js
@@ -53,8 +53,14 @@ function App() {
   return (
     <div className="App">
       <Banner></Banner>
-      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
-      {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria}></Time>)} {/*Para cada time em 'times', retorne a tag com nome*/}
+      <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      {times.map(time => <Time 
+        key={time.nome} 
+        nome={time.nome} 
+        corPrimaria={time.corPrimaria} 
+        corSecundaria={time.corSecundaria}
+        colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+        />)} {/*Para cada time em 'times', retorne a tag com nome e chave do mesmo valor*/}
     </div>
   );
 }
