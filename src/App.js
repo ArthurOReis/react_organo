65a3c35b5b08503920bfec73998b25df4aa33b43
ArthurOReis <arthuroreis2005@gmail.com>
2023-06-15 15:26:58 -0300

    Finalizando cards

--- src/App.js
+++ src/App.js
@@ -45,8 +45,9 @@ function App() {
   ];
 
   const [colaboradores, setColaboradores] = useState([]);
+
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    console.log(colaborador);
+    //debugger
     setColaboradores([...colaboradores, colaborador]);
   }
 
